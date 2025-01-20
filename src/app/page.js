3a4403529d6edd985757e64ff0473461e748c68c
Ishan Warshamana <ishanwarshamana@gmail.com>
2025-01-20 15:10:59 +0530

    Added hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import HeroSection from "./components/HeroSection";
+
 
 export default function Home() {
   return (
-    <div >
-      <h1>Portfolio Website</h1>
+    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <div className="container mx-auto px-12 py-4">
+      <HeroSection />
     </div>
+    </main>
   );
 }
